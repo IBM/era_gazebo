a19c47476fbb65be1984f4a6143cb8d12a9f4152
Akin Sisbot <easisbot@us.ibm.com>
2019-04-10 14:14:14 -0400

    ERAMsgBuilder consider tf prefix

--- src/ERAmsgBuilder.cpp
+++ src/ERAmsgBuilder.cpp
@@ -12,12 +12,13 @@ public:
 	ERAmsgBuilder(): tf(), target_frame("base_footprint") 
 	{
 		grid_sub.subscribe(n, "local_map", 100);
-		tf_filter = new tf::MessageFilter<nav_msgs::OccupancyGrid>(grid_sub, tf, target_frame, 100);
+		n.getParam("tf_prefix", tf_prefix);
+		tf_filter = new tf::MessageFilter<nav_msgs::OccupancyGrid>(grid_sub, tf, tf_prefix+"/"+target_frame, 100);
 		tf_filter->registerCallback( boost::bind(&ERAmsgBuilder::callback, this, _1) );
 		pub = n.advertise<era_gazebo::ERAMsg>("transmit_msg", 1000);
 
 		n.getParam("ERAmsgBuilder_node/ID", out_msg.ID);
-		ROS_ERROR_STREAM("here: " << out_msg.ID);
+		ROS_INFO_STREAM("ROBOT ID: " << out_msg.ID);
 	}
 private:
 	message_filters::Subscriber<nav_msgs::OccupancyGrid> grid_sub;
@@ -26,6 +27,7 @@ private:
 	ros::NodeHandle n;
 	ros::Publisher pub;
 	std::string target_frame;
+      	std::string tf_prefix;
 	era_gazebo::ERAMsg out_msg;
 
 	void callback(const boost::shared_ptr<const nav_msgs::OccupancyGrid>& grid_ptr) 
@@ -33,7 +35,7 @@ private:
 		tf::StampedTransform transform;
 		
     	try{
-      		 tf.lookupTransform("/map", "/base_footprint",  
+      		 tf.lookupTransform("/map", tf_prefix+"/base_footprint",  
                                 ros::Time(grid_ptr->header.stamp), transform);
     		}
     	catch (tf::TransformException ex){
