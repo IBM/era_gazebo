6c80c46eebae1ab4a14cfc52a42bdf7deaaccb38
Varun Mannam <vmannam@nd.edu>
2019-07-02 12:57:41 -0400

    Single file that handled both cases of object detection with and without mask. Added timing for the inference of each image in callback.

--- scripts/detection_tensorflow.py
+++ scripts/detection_tensorflow.py
@@ -49,7 +49,14 @@ import random
 #PATH_TO_CKPT = '~/catkin_ws/src/era_gazebo/models/' + MODEL_NAME + '/frozen_inference_graph.pb'
 
 # List of the strings that is used to add correct label for each box.
+device = '/gpu:0'      #'/cpu:0' #at present 1 device is assumed, Set to cpu if you are using cpu else set to gpu 
 
+if device == '/cpu:0':
+    os.environ["CUDA_VISIBLE_DEVICES"]="-1" # -1 set no GPU visible
+else:
+    os.environ["CUDA_VISIBLE_DEVICES"] = "0" # -1 set no GPU visible
+    
+mask_enabled = True #set to Flase for non-mask object detection and True for mask RCNN object detection
 
 NUM_CLASSES = 90
 
@@ -149,11 +156,6 @@ class ObjectDetectionTF():
         from object_detection.utils import label_map_util
         from object_detection.utils import visualization_utils as vis_util
 
-        self._sub = rospy.Subscriber('image_input', Image, self.imageCallback, queue_size=1)
-        self._pub = rospy.Publisher('detected_objects', DetectionBoxList, queue_size=1)
-        self.image_pub = rospy.Publisher("detection_image",Image, queue_size=1)
-        
-
         model_zoo_path = rospy.get_param('~model_zoo_path', '~/tensorflow/models')
         model_path = rospy.get_param('~model')
 
@@ -164,133 +166,147 @@ class ObjectDetectionTF():
         print("PATH TO LABELS " + PATH_TO_LABELS)
 
         #self.orb = cv2.xfeatures2d.SIFT_create()
-
+        
+        self.device = device
+        self.mask_enabled = mask_enabled
         self.matcher = ImageMatcher()
         
-        self.detection_graph = tf.Graph()
-        with self.detection_graph.as_default():
-            od_graph_def = tf.GraphDef()
-            with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
-                serialized_graph = fid.read()
-                od_graph_def.ParseFromString(serialized_graph)
-                tf.import_graph_def(od_graph_def, name='')
-                
+        with tf.device(self.device):
+            self.detection_graph = tf.Graph()
+            with self.detection_graph.as_default():
+                od_graph_def = tf.GraphDef()
+                with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+                    serialized_graph = fid.read()
+                    od_graph_def.ParseFromString(serialized_graph)
+                    tf.import_graph_def(od_graph_def, name='')
+                    
                 label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
                 categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
                 self.category_index = label_map_util.create_category_index(categories)
-
-                self.sess = tf.Session() 
-               
-
-    
                 
+                config = tf.ConfigProto() #log_device_placement=True)
+                
+                if self.device == '/gpu:0': #only for GPU this is valid
+                    config.gpu_options.allow_growth=True  #this will allocate the memory during the run time
+                    #to limit GPU memory of the card choose next option "per_process_gpu_memory_fraction"
+                    #config.gpu_options.per_process_gpu_memory_fraction = 0.6
+                self.sess = tf.Session(config=config) 
+                    
+            self._sub = rospy.Subscriber('image_input', Image, self.imageCallback, queue_size=1)
+            self._pub = rospy.Publisher('detected_objects', DetectionBoxList, queue_size=1)
+            self.image_pub = rospy.Publisher("detection_image",Image, queue_size=1)
+    
     def imageCallback(self, msg):
         #added imports for callback function
-        import object_detection
-        from object_detection.utils import ops
-
-        cv_image = self._cv_bridge.imgmsg_to_cv2(msg, "bgr8")
-
-        image = cv2.cvtColor(cv_image,cv2.COLOR_BGR2RGB)
-
-        image_np = np.asarray(image)
-        
-        image_np_expanded = np.expand_dims(image_np, axis=0)
-        
-        # Definite input and output Tensors for detection_graph
-        image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
-        
-        # Each box represents a part of the image where a particular object was detected.
-        detection_boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
-        # Each score represent how level of confidence for each of the objects.
-        # Score is shown on the result image, together with the class label.
-        detection_scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
-        detection_classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
-        num_detections = self.detection_graph.get_tensor_by_name('num_detections:0')
-        detection_masks = self.detection_graph.get_tensor_by_name('detection_masks:0') #included masks from TF graph 
-
-    # Actual detection.
-        (boxes, scores, classes, num, masks) = self.sess.run(
-            [detection_boxes, detection_scores, detection_classes, num_detections,detection_masks],
-            feed_dict={image_tensor: image_np_expanded})
-
-        boxes=np.squeeze(boxes)
-        classes =np.squeeze(classes)
-        scores = np.squeeze(scores)
-        masks = np.squeeze(masks) #squeeze masks similar to boxes
-
-        result_out = DetectionBoxList()
-        result_out.header = msg.header
-        dim = image.shape[0:2]
-        height, width = dim[0], dim[1]
-       
-        for bb, sc, cl, ma in zip(boxes,scores,classes,masks) :
-            mask_img = ma #mask of the object
-            if(sc > .30):
-                pose = DetectionBox()
-                pose.left = int(bb[1]*width)
-                pose.right = int(bb[3]*width)
-                pose.top = int(bb[0]*height)
-                pose.bottom = int(bb[2]*height)
-
-                pose.type = self.category_index[cl]['name']
-                
-                cropped_image = image[pose.top:pose.bottom, pose.left:pose.right]
-
-                pose.id = self.matcher.match_and_add_model(cropped_image, pose.type)
-                
-                #resize the mask accd to boxW and boxH
-                boxW = pose.right - pose.left #mask width
-                boxH = pose.bottom - pose.top #mask height
-                #interpolate mask of 15x15 to box dimensions and convert to binary mask
-                mask_img = cv2.resize(mask_img,(boxW,boxH),interpolation=cv2.INTER_NEAREST)
-                mask_img = (mask_img > 0.5) #binary mask of 0 or 1
-                #colors for the image masks
-                colors=[[0, 255, 0],[0, 0, 255],[255, 0, 0],[0, 255, 255],[255, 255, 0],[255, 0, 255],[80, 70, 180],[250, 80, 190],[245, 145, 50],[70, 150, 250],[50, 190, 190]]
-                #added few colors
-                colors = np.array(colors) #convert to array from list
-                color_selected = colors[np.random.randint(len(colors),size=1)] #randomly select one color for this object and value of the color
-                #mix the image with the color and update this only if the mask is enabled
-                img_seg_updated = ((0.4 * color_selected) + (0.6 * cropped_image)).astype("uint8") #combining the color with cropped image
-                for h in range(boxH): #box height
-                    for w in range(boxW): #box width
-                        if mask_img[h,w] == True: #if the mask is enabled then update the image with new mask generated
-                            image[pose.top+h,pose.left+w] = img_seg_updated[h,w] 
-
-                result_out.detection_list.append(pose)
-
-                cv2.rectangle(image, (pose.left, pose.top), (pose.right, pose.bottom), (255,0,0), 2, 1) #decreased the thickness from 4 to 2
-                cv2.putText(image, str(pose.id), ( int(pose.left+30), int(pose.top+30) ), cv2.FONT_HERSHEY_SIMPLEX, 2.0,(255,0,0),5)
-                
-                #kp1, des1 = self.orb.detectAndCompute(crop_image, None)
-                #cv2.drawKeypoints(crop_image,kp1, crop_image, color=(0,255,0), flags=0)
-
-                
-        if result_out.detection_list:
-            self._pub.publish(result_out)
-                
-            # Visualization of the results of a detection.
-            # vis_util.visualize_boxes_and_labels_on_image_array(
-            #     image,
-            #     np.squeeze(boxes),
-            #     np.squeeze(classes).astype(np.int32),
-            #     np.squeeze(scores),
-            #     self.category_index,
-            #     use_normalized_coordinates=True,
-            #     min_score_thresh=.8,
-            #     line_thickness=8)
+        #import object_detection
+        #from object_detection.utils import ops
+        with tf.device(self.device):
+            cv_image = self._cv_bridge.imgmsg_to_cv2(msg, "bgr8")
+            image = cv2.cvtColor(cv_image,cv2.COLOR_BGR2RGB)
+            image_np = np.asarray(image)
+            image_np_expanded = np.expand_dims(image_np, axis=0)
             
-        
-        
-            #img=cv2.cvtColor(image_np, cv2.COLOR_BRGB
-            image_out = self._cv_bridge.cv2_to_imgmsg(image,"rgb8")
-      
-            image_out.header = msg.header
-            self.image_pub.publish(image_out)
+            # Definite input and output Tensors for detection_graph
+            image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
+            
+            # Each box represents a part of the image where a particular object was detected.
+            detection_boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
+            # Each score represent how level of confidence for each of the objects.
+            # Score is shown on the result image, together with the class label.
+            detection_scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
+            detection_classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
+            num_detections = self.detection_graph.get_tensor_by_name('num_detections:0')
+            t1 = time.time()
+            if self.mask_enabled ==True:
+                detection_masks = self.detection_graph.get_tensor_by_name('detection_masks:0') #included masks from TF graph 
+                (boxes, scores, classes, num, masks) = self.sess.run(
+                [detection_boxes, detection_scores, detection_classes, num_detections,detection_masks],
+                feed_dict={image_tensor: image_np_expanded})
+            else:
+                t1 = time.time()
+                (boxes, scores, classes, num) = self.sess.run(
+                [detection_boxes, detection_scores, detection_classes, num_detections],
+                feed_dict={image_tensor: image_np_expanded})
+            t2 = time.time()
+            print(' >>>>> Model name is here: >>>>>>', self.device)
+            print(' >>>>> Model inference time (in sec): >>>>>> ', t2-t1)
 
-        #rospy.sleep(1)
+            boxes=np.squeeze(boxes)
+            classes =np.squeeze(classes)
+            scores = np.squeeze(scores)
+                       
+            if self.mask_enabled == True:
+                masks = np.squeeze(masks) #squeeze masks similar to boxes
+                
+            result_out = DetectionBoxList()
+            result_out.header = msg.header
+            dim = image.shape[0:2]
+            height, width = dim[0], dim[1]
+            
+            i = 0
+            for bb, sc, cl in zip(boxes,scores,classes) :
+                
+                if(sc > .30):
+                    pose = DetectionBox()
+                    pose.left = int(bb[1]*width)
+                    pose.right = int(bb[3]*width)
+                    pose.top = int(bb[0]*height)
+                    pose.bottom = int(bb[2]*height)
 
-        
+                    pose.type = self.category_index[cl]['name']
+                    cropped_image = image[pose.top:pose.bottom, pose.left:pose.right]
+                    pose.id = self.matcher.match_and_add_model(cropped_image, pose.type)
+                    
+                    if self.mask_enabled == True: #runs when only mask is enabled
+                        #resize the mask accd to boxW and boxH
+                        ma = masks[i]  #get the present mask
+                        boxW = pose.right - pose.left #mask width
+                        boxH = pose.bottom - pose.top #mask height
+                        #interpolate mask of 15x15 to box dimensions and convert to binary mask
+                        mask_img = cv2.resize(ma,(boxW,boxH),interpolation=cv2.INTER_NEAREST)
+                        
+                        #colors for the image masks
+                        colors=[[0, 255, 0],[0, 0, 255],[255, 0, 0],[0, 255, 255],[255, 255, 0],[255, 0, 255],[80, 70, 180],[250, 80, 190],[245, 145, 50],[70, 150, 250],[50, 190, 190]]
+                        #added few colors
+                        colors = np.array(colors) #convert to array from list
+                        color_selected = colors[np.random.randint(len(colors),size=1)] #randomly select one color for this object and value of the color
+                        #mix the image with the color and update this only if the mask is enabled
+                        img_seg_updated = ((0.4 * color_selected) + (0.6 * cropped_image)).astype("uint8") #combining the color with cropped image
+                        #writing the maksed image to original image
+                        for h in range(boxH): #box height
+                            for w in range(boxW): #box width
+                                if mask_img[h,w] >0.5: #if the mask is enabled then update the image with new mask generated
+                                    image[pose.top+h,pose.left+w] = img_seg_updated[h,w] 
+
+                    result_out.detection_list.append(pose)
+
+                    cv2.rectangle(image, (pose.left, pose.top), (pose.right, pose.bottom), (255,0,0), 2, 1) #decreased the thickness from 4 to 2
+                    cv2.putText(image, str(pose.id), ( int(pose.left+30), int(pose.top+30) ), cv2.FONT_HERSHEY_SIMPLEX, 2.0,(255,0,0),5)
+                i = i+1    #go to the next mask
+                    #kp1, des1 = self.orb.detectAndCompute(crop_image, None)
+                    #cv2.drawKeypoints(crop_image,kp1, crop_image, color=(0,255,0), flags=0)
+
+            if result_out.detection_list:
+                self._pub.publish(result_out)
+                    
+                # Visualization of the results of a detection.
+                # vis_util.visualize_boxes_and_labels_on_image_array(
+                #     image,
+                #     np.squeeze(boxes),
+                #     np.squeeze(classes).astype(np.int32),
+                #     np.squeeze(scores),
+                #     self.category_index,
+                #     use_normalized_coordinates=True,
+                #     min_score_thresh=.8,
+                #     line_thickness=8)
+            
+                #img=cv2.cvtColor(image_np, cv2.COLOR_BRGB
+                image_out = self._cv_bridge.cv2_to_imgmsg(image,"rgb8")
+                image_out.header = msg.header
+                self.image_pub.publish(image_out)
+
+            #rospy.sleep(1)
+
     def run(self):
         rospy.spin()
 
