c560daf9dc235a90e2ba4c39e6116f08bbcd77fc
SebastianC1492 <67912587+SebastianC1492@users.noreply.github.com>
2020-08-10 15:11:07 -0400

    Clean up

--- src/occgrid_node.cpp
+++ src/occgrid_node.cpp
@@ -1,12 +1,19 @@
 #include <ros/ros.h>
 #include <ros/console.h>
+#include <tf2_ros/transform_listener.h>
+//#include <tf2/convert.h>
+#include <tf2/utils.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include <nav_msgs/Odometry.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <nav_msgs/OccupancyGrid.h>
+#include <cstdio>
 #include <cmath>
+#include <string>
 #include "occgrid.h"
 
 //Global Variables
+std::string global_frame;
 bool rolling_window;
 double min_obstacle_height;
 double max_obstacle_height;
@@ -24,20 +31,18 @@ ros::Publisher occ_grid_pub;
 double rw, rx, ry, rz; //Components of quarternion that represents the orientation of robot
 double prev_angle;
 
-void quarternionMultiply(double px, double py, double pz, double pw, double qx, double qy, double qz, double qw) {
-    rw = (pw * qw) - (px * py) - (pz * qx) - (qy * qz);
-    rx = (pw * qx) + (qw * px) + (py * qz - pz * qy);
-    ry = (pw * qy) + (qw * py) + (pz * qx - px * qz);
-    rz = (pw * qz) + (qw * pz) + (px * qy - py * qx);
-}	
-
 void cloudCallback (const sensor_msgs::PointCloud2::ConstPtr& cloud) {
 	
     ROS_INFO("Received PointCloud");
     nav_msgs::OccupancyGrid occgrid;
 
-    //Convert cloud to an occupancy grid
+    //Transform cloud's frame to global frame
+    sensor_msgs::PointCloud2 global_cloud;
+    tf2_ros::Buffer buffer(ros::Duration(10));
+    tf2_ros::TransformListener tf(buffer);
+    buffer.transform(cloud, global_cloud, global_frame);
 
+    //Convert cloud to an occupancy grid
     float* cloud_data = (float*) cloud->data.data();
 
     unsigned int cloud_size = cloud->width * cloud->height;
@@ -57,35 +62,14 @@ void cloudCallback (const sensor_msgs::PointCloud2::ConstPtr& cloud) {
 
     //ROS_INFO("Occupancy Grid: <Height, Width, Resolution> = <%d, %d, %f>", occgrid.info.height, occgrid.info.width, occgrid.info.resolution);
 
-    occgrid.info.origin.position.x = 0.0 - (size_x / resolution);//odom.pose.pose.position.x - (size_x / resolution);
-    occgrid.info.origin.position.y = 0.0 - (size_y / resolution);//odom.pose.pose.position.y - (size_y / resolution);
-    occgrid.info.origin.position.z = 0.0;//odom.pose.pose.position.z;
+    occgrid.info.origin.position.x = 0.0 - (size_x / resolution); //odom.pose.pose.position.x - (size_x / resolution);
+    occgrid.info.origin.position.y = 0.0 - (size_y / resolution); //odom.pose.pose.position.y - (size_y / resolution);
+    occgrid.info.origin.position.z = 0.0; //odom.pose.pose.position.z;
 
-    /* IGNORE
-    double px = odom.pose.pose.orientation.x;
-    double py = odom.pose.pose.orientation.y;
-    double pz = odom.pose.pose.orientation.z;
-    double pw = odom.pose.pose.orientation.w;
-    double vx = cos(odom.twist.twist.angular.z);
-    double vy = sin(odom.twist.twist.angular.y);
-    
-    double sin_angle = sin((odom.twist.twist.angular.z - prev_angle) / 2);
-    double cos_angle = cos((odom.twist.twist.angular.z - prev_angle) / 2);
-    double cos_x = vx / sqrt(vx * vx + vy * vy);
-    double cos_y = vy / sqrt(vx * vx + vy * vy);
- 
-    double qx = sin_angle * cos_x;
-    double qy = sin_angle * cos_y;
-    double qz = 0.0; //Since vz ~ 0.0
-    double qw = cos_angle;
-
-    quarternionMultiply(-1 * qx, -1 * qy, -1 * qz, qw, odom.pose.pose.orientation.x, odom.pose.pose.orientation.y, odom.pose.pose.orientation.z, odom.pose.pose.orientation.w);
-    quarternionMultiply(rx, ry, rz, rw, qx, qy, qz, qw);*/
-
-    occgrid.info.origin.orientation.x = 0.0;//rx;//odom.pose.pose.orientation.x;
-    occgrid.info.origin.orientation.y = 0.0;//ry;//odom.pose.pose.orientation.y;
-    occgrid.info.origin.orientation.z = 0.0;//rz;//odom.pose.pose.orientation.z;
-    occgrid.info.origin.orientation.w = 1.0;//rw;//odom.pose.pose.orientation.w;
+    occgrid.info.origin.orientation.x = 0.0; //rx;//odom.pose.pose.orientation.x;
+    occgrid.info.origin.orientation.y = 0.0; //ry;//odom.pose.pose.orientation.y;
+    occgrid.info.origin.orientation.z = 0.0; //rz;//odom.pose.pose.orientation.z;
+    occgrid.info.origin.orientation.w = 1.0; //rw;//odom.pose.pose.orientation.w;
 
     //ROS_INFO("Occupancy Grid Odometry: x, y, z = %f, %f, %f", odom.pose.pose.position.x - (size_x / resolution), odom.pose.pose.position.y - (size_y / resolution), occgrid.info.origin.position.z);
 
@@ -107,7 +91,7 @@ void odomCallback (const nav_msgs::Odometry::ConstPtr& odometry) {
    odom.pose.pose.orientation.w = odometry->pose.pose.orientation.w;
 
    odom.twist.twist.angular.z = odometry->twist.twist.angular.z;
-   ROS_INFO("Yaw = %f", odom.twist.twist.angular.z);
+   //ROS_INFO("Yaw = %f", odom.twist.twist.angular.z);
 }
 
 int main (int argc, char** argv) {
@@ -121,6 +105,7 @@ int main (int argc, char** argv) {
     ros::Subscriber odometry_sub = n.subscribe("/carla/hero1/odometry", 10, odomCallback);
     occ_grid_pub = n.advertise<nav_msgs::OccupancyGrid>("local_map", 1000);
 
+    ros::param::param<std::string>("global_frame", global_frame, "map");
     ros::param::param<double>("resolution", resolution, 2.0); //Default value to be 2 meters per cell
     ros::param::param<bool>("rolling_window", rolling_window, false);
     ros::param::param<double>("min_obstacle_height", min_obstacle_height, 0.05);
